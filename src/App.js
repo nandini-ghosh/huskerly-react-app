3b609cb4b4697beb250c3c2aa1722ccc9d2e89a6
nandini-ghosh <nandini.ghosh2094@gmail.com>
2024-07-17 14:34:29 -0400

    Ignore that, we're going back to cat HTTPS

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
 
   const handleClick = async () => {
     try {
-      const response = await fetch('https://cors-anywhere.herokuapp.com/https://http.cat/102');
+      const response = await fetch('https://http.cat/102');
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
