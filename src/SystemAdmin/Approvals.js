2a361fec0b4269d0d2beb6d209f5ed2bdcffa370
nandini-ghosh <nandini.ghosh2094@gmail.com>
2024-07-26 10:31:47 -0400

    Approvals page complete

--- src/SystemAdmin/Approvals.js
+++ src/SystemAdmin/Approvals.js
@@ -9,12 +9,33 @@ function Approvals() {
         <HomeLayout>
             <body id='charcoal-background'>
                 <InnerHomeLayout>
-                    <SettingsSidebar/>
+                    <SettingsSidebar />
                     <div className='blockbox'>
-                    <SubheaderRegular text="Pending Approvals"></SubheaderRegular>
-                    <div className='content-box'>
-                        
-                    </div>
+                        <SubheaderRegular text="Pending Approvals"></SubheaderRegular>
+                        <div className='content-box-wrapper'>
+                            <div className='content-box'>
+                                <div className='list'>
+                                    <div className='list-item'>
+                                        <div className='list-item-text-wrapper'>
+                                            Fintech Startup
+                                        </div>
+                                        <div className='list-item-button-wrapper'>
+                                            <button className='button-red wd-small'>Approve</button>
+                                            <button className='button-white wd-small'>Deny</button>
+                                        </div>
+                                    </div>
+                                    <div className='list-item'>
+                                    <div className='list-item-text-wrapper'>
+                                            Huntington News
+                                        </div>
+                                        <div className='list-item-button-wrapper'>
+                                            <button className='button-red wd-small'>Approve</button>
+                                            <button className='button-white wd-small'>Deny</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </InnerHomeLayout>
             </body>
