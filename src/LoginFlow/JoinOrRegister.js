59fb516378e4e3517606798a0565af2093eca02a
nandini-ghosh <nandini.ghosh2094@gmail.com>
2024-07-31 16:44:38 -0400

    navigating to approvals for System Admins

--- src/LoginFlow/JoinOrRegister.js
+++ src/LoginFlow/JoinOrRegister.js
@@ -1,7 +1,7 @@
 import './style.css';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { fetchUserAttributes, getCurrentUser, signOut } from 'aws-amplify/auth';
 
@@ -19,6 +19,7 @@ function JoinOrRegister() {
     });
 
     const [userAtts, setUserAtts] = useState();
+    const navigate = useNavigate();
 
     // Get the stuff from Cognito 
     useEffect(() => {
@@ -32,8 +33,17 @@ function JoinOrRegister() {
                 const uA = await fetchUserAttributes();
                 setUserAtts(uA);
                 console.log(uA)
-                console.log(uD)
-        
+                console.log(uA.email);
+                console.log(uD);
+
+                //check if the current user is a system admin and navigate to the approvals page instead
+                if (uA) {
+                    const permissions = await getUserPermissions(uA.email);
+                    if (permissions==='"SYS_ADMIN"') {
+                        navigate('/approvals'); // Navigate to the Approvals component
+                    }
+                }
+
             } catch (error) {
                 console.error('Error fetching user:', error);
             }
@@ -41,26 +51,49 @@ function JoinOrRegister() {
         fetchUser();
     }, []);
 
+
+    // Function to fetch user permissions for the current user. Returns a string that represents the user group
+    async function getUserPermissions(email) {
+        const url = `https://7hbu1e48i3.execute-api.us-east-2.amazonaws.com/user/users/permission/${email}/1`;
+
+        try {
+            //fetch the user's permission group
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const text = await response.text(); 
+            console.log(text);
+
+            // Return the permission group text
+            return text;
+        } catch (error) {
+            console.error(error.message);
+            return null;
+        }
+    }
+
     const handleLogOutClick = () => {
         console.log("Log out");
-        //signOut();
+       //signOut();
     };
 
     return (
         <Authenticator>
-        <body id="white-background">
-            <div className='dialogue-box'>
-                <div className='dialogue-box-content'>
-                    <div className='dialogue-box-text'>
-                        Welcome to Huskerly!
-                        <br></br>What would you like to do today?
+            <body id="white-background">
+                <div className='dialogue-box'>
+                    <div className='dialogue-box-content'>
+                        <div className='dialogue-box-text'>
+                            Welcome to Huskerly!
+                            <br></br>What would you like to do today?
+                        </div>
+                        <Link to={{ pathname: "/join" }} state={{ userData, userAtts }}><div className='button-white-outline wd-large spacing-small'  >Join an existing group</div> </Link>
+                        <Link to={{ pathname: "/register" }} state={{ userData, userAtts }}><div className='button-white-outline wd-large spacing-small'>Register a new group</div> </Link>
+                        <button className='button-white-outline wd-large spacing-small' onClick={handleLogOutClick()} >Temp Log Out</button>
                     </div>
-                    <Link to={{pathname: "/join"}} state={{userData, userAtts}}><div className='button-white-outline wd-large spacing-small'  >Join an existing group</div> </Link>
-                    <Link to={{ pathname: "/register" }} state={{userData, userAtts}}><div className='button-white-outline wd-large spacing-small'>Register a new group</div> </Link>
-                    <button className='button-white-outline wd-large spacing-small' onClick={handleLogOutClick()} >Temp Log Out</button>
                 </div>
-            </div>
-        </body>
+            </body>
         </Authenticator>
     );
 }
