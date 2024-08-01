efef704f9d5943fa7c0292460025aebe3d5a9692
nandini-ghosh <nandini.ghosh2094@gmail.com>
2024-08-01 15:52:09 -0400

    endpoints for requesting an org and viewing pending org requests. also added additional js for form submissions in Register

--- src/LoginFlow/Register.js
+++ src/LoginFlow/Register.js
@@ -2,16 +2,67 @@ import './style.css';
 import { FaUserCircle } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
-
-
+import { useEffect } from 'react';
 
 function Register() {
     const location = useLocation();
-    const userData = location.state.userData
-    const userAtts = location.state.userAtts
+    const userData = location.state?.userData
+    const userAtts = location.state?.userAtts
 
     console.log(userData)
     console.log(userAtts)
+
+    useEffect(() => {
+        const input = document.getElementById('groupname');
+        const btn = document.getElementById('register-btn');
+
+        if (input && btn) {
+            const handleKeyUp = (e) => {
+                const value = e.currentTarget.value;
+                btn.disabled = value === "";
+            };
+
+            input.addEventListener('keyup', handleKeyUp);
+
+            // Cleanup function to remove event listener
+            return () => {
+                input.removeEventListener('keyup', handleKeyUp);
+            };
+        }
+    }, []);
+
+    // Function to post group name and user email to request organization registration
+    async function postRequest(e) {
+        e.preventDefault(); // Prevent the form from submitting
+        const inputValue = document.getElementById('groupname').value;
+        const userEmail = userAtts.email;
+        console.log(inputValue);
+        console.log(userEmail);
+        const url = `https://7hbu1e48i3.execute-api.us-east-2.amazonaws.com/user/org/request`;
+
+        try {
+            const response = await fetch(url, {
+                mode: 'no-cors',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ org_name: inputValue, creator_email: userEmail })
+            });
+
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const text = await response.text();
+            console.log(text);
+
+        } catch (error) {
+            console.error(error.message);
+            return null;
+        }
+    }
+
 
     return (
         <body id="white-background">
@@ -19,14 +70,16 @@ function Register() {
                 <div className='user-icon-xl'><FaUserCircle /></div>
                 <div className='caption-wrapper'>
                     <div className='caption-text username'> Welcome {userAtts.name}! </div>
-                    <div className='caption-text email'> { userAtts.email } </div>
+                    <div className='caption-text email'> {userAtts.email} </div>
                 </div>
-                <div className='organization-list-wrapper'>
+                <form className='organization-list-wrapper' onSubmit={postRequest}>
                     <div className='caption-text'>Group Name</div>
                     <input id="groupname" className='input-field wd-large' type='text' placeholder='Enter a name for your group...'></input>
-                </div>
-                <Link to="/registration-confirmation"><div className='button-black wd-large spacing-large'>Register group</div></Link>
-                <Link to="/join" state={{userData, userAtts}}><div className='link-text'>Join an existing group instead</div></Link>
+
+                    <button id='register-btn' className='button-black wd-large spacing-large' disabled>Register group</button>
+                    {/* <Link to="/registration-confirmation"><button id='register-btn' className='button-black wd-large spacing-large' onClick={postRequest} disabled>Register group</button></Link> */}
+                </form>
+                <Link to="/join" state={{ userData, userAtts }}><div className='link-text'>Join an existing group instead</div></Link>
             </div>
         </body>
     );
--- src/SystemAdmin/Approvals.js
+++ src/SystemAdmin/Approvals.js
@@ -1,10 +1,38 @@
 import './style.css';
+import { useEffect, useState } from 'react';
 import HomeLayout from '../BuilderComponents/HomeLayout';
 import InnerHomeLayout from '../BuilderComponents/InnerHomeLayout';
 import SettingsSidebar from '../BuilderComponents/SettingsSidebar';
 import SubheaderRegular from '../BuilderComponents/SubheaderRegular';
 
 function Approvals() {
+
+    const [approvals, setApprovals] = useState([]);
+
+    //Fetch all organizations pending approvals for the System Admin
+    async function getPendingApprovals() {
+        const url = `https://7hbu1e48i3.execute-api.us-east-2.amazonaws.com/user/org/requests`;
+
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const json = await response.json();
+            console.log(json);
+
+            setApprovals(json);
+        } catch (error) {
+            console.error(error.message);
+            return null;
+        }
+    }
+
+    useEffect(() => {
+        getPendingApprovals();
+    }, []);
+
     return (
         <HomeLayout>
             <body id='charcoal-background'>
@@ -15,24 +43,19 @@ function Approvals() {
                         <div className='content-box-wrapper'>
                             <div className='content-box'>
                                 <div className='list'>
-                                    <div className='list-item'>
-                                        <div className='list-item-text-wrapper'>
-                                            Fintech Startup
+                                    {/* For each organization pending approval, render a component */}
+                                    {approvals.map((approval, index) => (
+                                        <div className='list-item' key={index}>
+                                            <div className='list-item-text-wrapper'>
+                                                {approval[0]}
+                                                <div className='list-item-email-text'>{approval[1]}</div>
+                                            </div>
+                                            <div className='list-item-button-wrapper'>
+                                                <button className='button-red wd-small'>Approve</button>
+                                                <button className='button-white wd-small'>Deny</button>
+                                            </div>
                                         </div>
-                                        <div className='list-item-button-wrapper'>
-                                            <button className='button-red wd-small'>Approve</button>
-                                            <button className='button-white wd-small'>Deny</button>
-                                        </div>
-                                    </div>
-                                    <div className='list-item'>
-                                    <div className='list-item-text-wrapper'>
-                                            Huntington News
-                                        </div>
-                                        <div className='list-item-button-wrapper'>
-                                            <button className='button-red wd-small'>Approve</button>
-                                            <button className='button-white wd-small'>Deny</button>
-                                        </div>
-                                    </div>
+                                    ))}
                                 </div>
                             </div>
                         </div>
