4d3088858a027360f4ecd0a80cf85f8caff060de
nandini-ghosh <nandini.ghosh2094@gmail.com>
2024-08-12 10:32:32 -0400

    added home screen setting button

--- src/BuilderComponents/SettingsSidebar.js
+++ src/BuilderComponents/SettingsSidebar.js
@@ -15,6 +15,9 @@ function SettingsSidebar() {
 
     const [userAtts, setUserAtts] = useState();
     const [userRole, setUserRole] = useState();
+    const [orgCount, setOrgCount] = useState();
+    const [orgName, setOrgName] = useState();
+    const [orgId, setOrgId] = useState();
 
     const location = useLocation();
 
@@ -29,6 +32,25 @@ function SettingsSidebar() {
                 // Get user's attributes
                 const uA = await fetchUserAttributes();
                 setUserAtts(uA);
+                setOrgId(uA["custom:OrgId"]);
+
+                // Fetch organization info if orgId is available
+                if (uA["custom:OrgId"]) {
+                    const url = `https://7hbu1e48i3.execute-api.us-east-2.amazonaws.com/message/org/${uA["custom:OrgId"]}`;
+                    const response = await fetch(url);
+
+                    if (!response.ok) {
+                        throw new Error(`Response status: ${response.status}`);
+                    }
+
+                    const json = await response.json();
+                    setOrgCount(json.Data.user_count);
+                    setOrgName(json.Data.name);
+                    
+                    // Log the organization details for debugging
+                    console.log(json.Data.name);
+                    console.log(json.Data.user_count);
+                }
 
                 // Check if the current user is a system admin and update state
                 if (uA) {
@@ -71,11 +93,11 @@ function SettingsSidebar() {
                 <Link to="/profile" state={{ userData, userAtts, userRole }}><div id="user-settings" className={`icon-sidebar white ${location.pathname === '/profile' ? 'active' : ''}`}><FaUserCircle /></div></Link>
             </div>
             <div className='icons-sidebar-wrapper'>
-                {/* <Link to={"/home"} state={{userData, userAtts, orgId, orgName, orgCount}}>
+                <Link to={"/home"} state={{userData, userAtts, orgId, orgName, orgCount}}>
                     <div id="home-settings" className={`icon-sidebar ${userRole === "SYS_ADMIN" ? 'hidden' : ''} ${location.pathname === '/home' ? 'active' : ''}`}>
                         <FaHome />
                     </div>
-                </Link> */}
+                </Link>
                 <Link to={"/approvals"}>
                     <div id="approval-settings" className={`icon-sidebar ${userRole === "SYS_ADMIN" ? '' : 'hidden'} ${location.pathname === '/approvals' ? 'active' : ''}`}>
                         <BsFillCheckSquareFill />
